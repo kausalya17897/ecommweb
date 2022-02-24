98de198d06f72eb30e0362a37e022dda534fa344
Kausalya.K <85003124+kausalya17897@users.noreply.github.com>
2022-02-25 00:12:06 +0530

    slideshow

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,12 +1,13 @@
 import { ArrowLeftOutlined, ArrowRightRounded } from "@mui/icons-material"
-import { imageListClasses } from "@mui/material"
+import { Slideritem } from "../pages/Slideritem"
 import styled from "styled-components"
+import { useState } from "react"
 
 const Container=styled.div`
 width:100%;
 height:100vh;
 display:flex;
-
+overflow:hidden;
 position:relative
 `
 const Arrow=styled.div`
@@ -20,40 +21,85 @@ position:absolute;
 top:0;
 margin:auto;
 bottom:0;
-left:${props=>props.direction==="left" &&"10px"};
-right:${props=>props.direction==="right" &&"10px"};
+left:${(props)=>props.direction==="left" &&"10px"};
+right:${(props)=>props.direction==="right" &&"10px"};
 cursor:pointer;
 opacity:0.5;
+z-index:2;
 `
 const Wrapper=styled.div`
-height:100%;`
-const Slide=styled.div`
+height:100%;
 display:flex;
-align-items:center`
-const ImageContainer=styled.div`
-flex:1;`
-const Info=styled.div`
-flex:1;`
-const Image=styled.img`
-`;
+transition:all 1.5s ease;
+transform:translateX(${(props)=>props.slideshow *-100}vw)`
+const Slide=styled.div`
+width:100vw;
+height:100vh;
+display:flex;
 
+background-color:${(props)=>props.bg};
+align-items:center;`
+const ImageContainer=styled.div`
+height:100%;
+width:60%;`
+const Info=styled.div`
+display:flex;
+flex-direction:column;
+height:60%;
+width:60%;
+padding:10px;
+margin-top:100px`
+const Image=styled.img`
+height:80%;
+`;
+const Title=styled.h1`
+font-size:70px;
+
+margin-top:0px;
+margin-bottom:0px;
+text-align:left;`
+const Description=styled.p`
+font-size:20px;
+font-weight:400;
+letter-spacing:3px;
+text-align:left;`
+const Button=styled.button`
+align-items:flex-start;
+padding:10px;
+width:150px;
+font-size:20px;
+background-color:transparent;
+cursor:pointer`
 export default function Slider(){
+    const handleClick=(direction)=>{
+        if(direction==="left"){
+            setSlideshow(slideshow>0?slideshow-1:2)
+        }else{
+            setSlideshow(slideshow<2?slideshow+1:0)
+        }
+    }
+    const [slideshow,setSlideshow]=useState(0)
     return(
         <Container>
-            <Arrow direction="left">
+            <Arrow direction="left" onClick={()=>handleClick("left")}>
                 <ArrowLeftOutlined style={{alignItems:"center",paddingTop:"11px"}}/>
             </Arrow>
-            <Wrapper>
-                <Slide>
-              <ImageContainer>
-                  <img src="./Image/image2.jpg" alt="dsd"/>
-              </ImageContainer>
-              <Info>
-
-              </Info>
-              </Slide>
+            <Wrapper slideshow={slideshow}>
+                {Slideritem.map((a)=>(
+ <Slide bg={a.bg} key={a.id} style={{backgroundColor:"{a.bg}"}}>
+ <ImageContainer>
+     <Image src={a.img}/>
+ </ImageContainer>
+ <Info>
+     <Title>{a.title}</Title>
+     <Description>{a.description}</Description>
+     <Button>SHOP NOW</Button>
+ </Info>
+ </Slide>
+                ))}
+              
             </Wrapper>
-            <Arrow direction="right">
+            <Arrow direction="right"onClick={()=>handleClick("right")}>
                 <ArrowRightRounded style={{alignItems:"center",paddingTop:"11px"}}/>
             </Arrow>
         </Container>
---
+++ src/pages/Slideritem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+export const Slideritem=[
+    {
+        id:1,
+        img:"https://th.bing.com/th/id/OIP.Hrs9nlfDI5hBhSZV4nqveAHaIp?pid=ImgDet&w=735&h=858&rs=1",
+        title:"SUMMER SALE",
+        description:"FLAT 40% OFF ON SALE ON NEW ARRIVALS FOR SUMMER",
+        bg:"#FEFEFE",
+    },
+    {
+        id:2,
+        img:"https://cdn.shopify.com/s/files/1/2162/5203/products/ROSEMARY_DRESS5272_v1_copy_800x.jpg?v=1578428048",
+        title:"WINTER SALE",
+        description:"FLAT 40% OFF ON SALE ON NEW ARRIVALS FOR SUMMER",
+        bg:"rgb(178,210,218)",
+    },
+    {
+        id:3,
+        img:"https://cdn.shopify.com/s/files/1/1003/9198/products/BeccaCannesFront_1200x.jpg?v=1571611661",
+        title:"POPULAR SALE",
+        description:"FLAT 40% OFF ON SALE ON NEW ARRIVALS FOR SUMMER",
+        bg:"#fb95bf",
+    },
+    
+   
+
+]
